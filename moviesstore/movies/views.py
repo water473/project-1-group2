ad86ec4a94f4e9c58610dab63ddf31d2d72e0310
James Xiao <1xiaojames@gmail.com>
2025-02-03 18:36:54 -0500

    implemented chpt9: movie reviews

--- moviesstore/movies/views.py
+++ moviesstore/movies/views.py
@@ -1,27 +1,6 @@
-from django.shortcuts import render
-from .models import Movie
-
-# Create your views here.
-
-# Do not un-comment this section. Leaving it for future reference.
-# movies = [
-#     {
-#         'id': 1, 'name': 'Inception', 'price': 12,
-#         'description': 'A mind-bending heist thriller.'
-#     },
-#     {
-#         'id': 2, 'name': 'Avatar', 'price': 13,
-#         'description': 'A journey to a distant world and the battle for resources.'
-#     },
-#     {
-#         'id': 3, 'name': 'The Dark Knight', 'price': 14,
-#         'description': 'Gothams vigilante faces the Joker.'
-#     },
-#     {
-#         'id': 4, 'name': 'Titanic', 'price': 11,
-#         'description': 'A love story set against the backdrop of the sinking Titanic.',
-#     }
-# ]
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Movie, Review
+from django.contrib.auth.decorators import login_required
 
 def index(request):
     search_term = request.GET.get('search')
@@ -29,14 +8,56 @@ def index(request):
         movies = Movie.objects.filter(name__icontains=search_term)
     else:
         movies = Movie.objects.all()
+
     template_data = {}
     template_data['title'] = 'Movies'
     template_data['movies'] = movies
     return render(request, 'movies/index.html', {'template_data': template_data})
 
 def show(request, id):
-    movie = Movie.objects.get(id = id)
+    movie = Movie.objects.get(id=id)
+    reviews = Review.objects.filter(movie=movie)
+
     template_data = {}
     template_data['title'] = movie.name
     template_data['movie'] = movie
+    template_data['reviews'] = reviews
     return render(request, 'movies/show.html', {'template_data': template_data})
+
+@login_required
+def create_review(request, id):
+    if request.method == 'POST' and request.POST['comment'] != '':
+        movie = Movie.objects.get(id=id)
+        review = Review()
+        review.comment = request.POST['comment']
+        review.movie = movie
+        review.user = request.user
+        review.save()
+        return redirect('movies.show', id=id)
+    else:
+        return redirect('movies.show', id=id)
+
+@login_required
+def edit_review(request, id, review_id):
+    review = get_object_or_404(Review, id=review_id)
+    if request.user != review.user:
+        return redirect('movies.show', id=id)
+
+    if request.method == 'GET':
+        template_data = {}
+        template_data['title'] = 'Edit Review'
+        template_data['review'] = review
+        return render(request, 'movies/edit_review.html', {'template_data': template_data})
+    elif request.method == 'POST' and request.POST['comment'] != '':
+        review = Review.objects.get(id=review_id)
+        review.comment = request.POST['comment']
+        review.save()
+        return redirect('movies.show', id=id)
+    else:
+        return redirect('movies.show', id=id)
+
+@login_required
+def delete_review(request, id, review_id):
+    review = get_object_or_404(Review, id=review_id, user=request.user)
+    review.delete()
+    return redirect('movies.show', id=id)
