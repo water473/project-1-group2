cc6e2c87c8a8a51d5230ae34475e6dcf17c89d7d
Dimitrios Mahairas <dimitriosmahairas@lawn-128-61-79-51.lawn.gatech.edu>
2025-02-05 11:49:07 -0500

    Added clear cart button which clears the users cart. Chapter 10 complete

--- moviesstore/cart/urls.py
+++ moviesstore/cart/urls.py
@@ -3,4 +3,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='cart.index'),
     path('<int:id>/add/', views.add, name='cart.add'),
+    path('clear/', views.clear, name='cart.clear'),
 ]
--- moviesstore/cart/views.py
+++ moviesstore/cart/views.py
@@ -24,3 +24,7 @@ def add(request, id):
     cart[id] = request.POST['quantity']
     request.session['cart'] = cart
     return redirect('cart.index')
+
+def clear(request):
+    request.session['cart'] = {}
+    return redirect('cart.index')
