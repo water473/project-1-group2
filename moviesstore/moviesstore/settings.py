e3baef6e9cbe0ac48f83e61bd5d86062dc49ff1c
Dimitrios Mahairas <dimitriosmahairas@lawn-128-61-72-83.lawn.gatech.edu>
2025-02-15 18:37:19 -0500

    Fixed background image and made some changes to make it easier to change pythonanywhere link

--- moviesstore/moviesstore/settings.py
+++ moviesstore/moviesstore/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = 'django-insecure-!)e2pqy(dab(b!aa^w(s9*-fi7@$rs3l#1=4(r-$e43kqar*_b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -127,6 +127,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
